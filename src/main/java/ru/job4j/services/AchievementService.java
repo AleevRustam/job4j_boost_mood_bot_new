1f72de51eb5eaa29346668cc346be7edf13ff4e0
AleevRustam <re1973@mail.ru>
2024-11-25 12:42:13 +0200

    @Value | application.properties [#505293 #521522]

--- src/main/java/ru/job4j/services/AchievementService.java
+++ src/main/java/ru/job4j/services/AchievementService.java
@@ -1,7 +1,19 @@
 package ru.job4j.services;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AchievementService {
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Bean is going through @PostConstruct init.");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Bean will be destroyed via @PreDestroy.");
+    }
 }
