284743babc92980754b1ff6f44c9c03001d45520
AleevRustam <re1973@mail.ru>
2024-11-28 11:41:11 +0200

    RemindService | Тестирование [#505308 #523800]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -27,18 +27,9 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
-//    @Bean
-//    public CommandLineRunner initTelegrammApi(ApplicationContext ctx) {
-//        return args -> {
-//            var bot = ctx.getBean(TelegramBotService.class);
-//            bot.receive(new Content());
-//        };
-//    }
-
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-            //var bot = ctx.getBean(TgRemoteService.class);
             var bot = ctx.getBean(TelegramBotService.class);
             var botsApi = new TelegramBotsApi(DefaultBotSession.class);
             try {
--- src/main/java/ru/job4j/repositories/UserRepository.java
+++ src/main/java/ru/job4j/repositories/UserRepository.java
@@ -10,8 +10,4 @@ import java.util.List;
 public interface UserRepository extends CrudRepository<User, Long> {
 
     List<User> findAll();
-
-//    User findByClientId(Long clientId);
-//
-//    void add(User user);
 }
